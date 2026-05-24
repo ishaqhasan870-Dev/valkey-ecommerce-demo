6b4cda5110627f97fed520823e05f05dfd726dd2
ishaghasan870-Dev <ishaqhasan870@gmail.com>
2026-05-24 17:21:46 +0530

    Upgrade VAL-HYD ecommerce platform

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import 'select2/dist/js/select2.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
+import "./commerce/commerce.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <React.StrictMode>
     <App />
-  </>
+  </React.StrictMode>
 );
 
 reportWebVitals();
